225d0148e902192850ad41e923ad0b64cf77932b
JosePaniagua <jose.paniagua.gomez@gmail.com>
2020-09-07 23:04:59 +0200

    Remove unnecessary comment

--- index.js
+++ index.js
@@ -13,27 +13,3 @@ module.exports = {
         }
     }
 };
-
-// fs.readFile('./addresses.csv', 'utf-8')
-//     .then((data) => {
-//         const lines = data.split('\n');
-//         const target_contract = lines.shift().split(',');
-//         const parsed_data = [];
-//         const errors = [];
-
-//         lines.forEach((line, index) => {
-//             const line_attributes = line.split(',');
-//             if (line_attributes.length < target_contract.length) {
-//                 errors.push({
-//                     'description': `Line ${(index + 1)} has too few arguments`
-//                 });
-//             } else {
-//                 const target_object = {};
-//                 target_contract.forEach((key, index) => target_object[key] = line_attributes[index]);
-//                 parsed_data.push(target_object);
-//             }
-//         });
-//         const response = { parsed_data, errors }
-//         fs.writeFile('./parsed.json', JSON.stringify(response), (err, data) => console.log('finish'));
-
-//     });
