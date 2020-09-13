a76db03a48f9000b80725892a6a77a416e2dc2e4
JosePaniagua <jose.paniagua.gomez@gmail.com>
2020-09-13 18:18:54 +0200

    CsvParser is ready to retun schema

---
+++ CsvParsers/CsvParserWithSchema.js
@@ -0,0 +1,39 @@
+const { CsvParser } = require('./CsvParser');
+const DataContractParser = require('../DataContractParser');
+
+class CsvParserWithSchema extends CsvParser {
+
+    constructor() {
+        super();
+    }
+
+    parse = async (file_path, schema) => {
+        const raw_data = await this.readData(file_path);
+        const lines = raw_data.split('\n');
+        const headers = lines.shift().split(',');
+        const indexed_schema = DataContractParser.getContractIndexes(schema, headers);
+        const rows = [];
+
+        lines.forEach((line) => {
+            const line_attributes = line.split(',');
+            const parsedLine = parseLine(line_attributes, indexed_schema);
+            rows.push(parsedLine);
+        });
+
+        return { rows };
+    }
+}
+
+
+const parseLine = (line_attributes, schema) => {
+    const line_object = {};
+    Object.entries(schema).forEach(([key, value]) => {
+        const parser = value.hasOwnProperty('source_index') ? getAttributeValue : parseLine;
+        line_object[key] = parser(line_attributes, value);
+    });
+    return line_object;
+};
+
+const getAttributeValue = (line_attributes, { source_index }) => line_attributes[source_index]
+
+module.exports = { CsvParserWithSchema };
